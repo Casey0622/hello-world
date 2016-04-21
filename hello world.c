e24724967496bd3a5925742585bfc5fd7b1a2f01
CaseyHui <casey_0622@163.com>
2016-04-22 01:20:27 +0800

    Update hello world.c

--- hello world.c
+++ hello world.c
@@ -4,3 +4,8 @@ int main()
 {
   cout <<"hello world!"<<endl;
 }
+/*************************************
+ *  This is my first code in github  *
+ *  I will push some PAT's coding in *
+ *  this town ,......  good luck!!   *
+ * **********************************/
